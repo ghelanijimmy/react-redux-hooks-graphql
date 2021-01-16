d6a8aa5272b487d3b49f9bd030074cd449534e0b
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2021-01-16 10:07:01 -0500

    added redux and implemented user reducer and actions to header and app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
