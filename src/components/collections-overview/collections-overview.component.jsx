23bf6ccf01fed5caebc03be980e06ace778a4380
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2021-01-17 13:15:34 -0500

    fix collection overview bug not rendering

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../preview-collection/collection-preview.component";
 import collectionsOverviewStyles from "./collections-overview.styles.module.scss";
 
 const CollectionsOverview = () => {
-  const shopCollections = useSelector(selectShopCollections);
+  const shopCollections = useSelector(selectCollectionsForPreview);
   return (
     <div className={collectionsOverviewStyles.collectionsOverview}>
       {shopCollections.map(({ id, title, items, routeName }) => (
